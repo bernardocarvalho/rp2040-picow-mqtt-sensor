cc98fdc2e13ea310fda86b2e8ba5df5f783bffe3
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2022-11-19 00:41:27 +0000

    Added MQTT support
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # rp2040-picow-mqtt-sensor
-Raspberry PI Pico W board PlatformIO Project
+Raspberry PI Pico W board PlatformIO Project 
+
+## Dependencies
+- [Raspberry Pi Pico W](https://www.raspberrypi.com/products/raspberry-pi-pico/?variant=raspberry-pi-pico-w), 
+- a $6 microcontroller board with WiFi
+- [PlatformIO](https://platformio.org/platformio-ide), an extension for [VSCode](https://code.visualstudio.com) 
+- with the most robust and reliable Arduino developer experience. Also works in CLI
+- [`arduino-pico`](https://github.com/earlephilhower/arduino-pico): a community-developed Arduino core for Pico/Pico W 
+- and other RP2040 boards with constant updates and excellent support for existing Arduino code
+
+## Inpsired by 
+
+- [PicoW_HomeAssistant_Starter](https://github.com/daniloc/PicoW_HomeAssistant_Starter), 
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,21 @@
 /*
  *
  * vim: sta:et:sw=4:ts=4:sts=4
+ * https://arduinojson.org/v6/how-to/use-arduinojson-with-arduinomqttclient/
  *******************************************************************************/
 
 #include <Arduino.h>
 #include <WiFi.h>
+//#include <time.h>
 
 #include <NTPClient.h>
 #include <PubSubClient.h>
 
+#include <ArduinoMqttClient.h>
+#include <ArduinoJson.h>
+
 #ifndef STASSID
+#include "arduino_secrets.h"
 // #define STASSID "TP-Link_D99E"
 // #define STAPSK "06189136"
 #define STASSID "Cabovisao-E30F"
@@ -21,7 +27,7 @@ const char *password = STAPSK;
 
 const char* mqtt_server = "test.mosquitto.org";
 const int mqtt_port = 1883;  // Sets the server details.
-// const char* mqtt_server = "91.121.93.94";
+                             // const char* mqtt_server = "91.121.93.94";
 const char* ntpServer = "ntp1.tecnico.ulisboa.pt";
 
 WiFiClient wClient;
@@ -32,9 +38,12 @@ WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP);
 
 PubSubClient mqttClient(wClient);
+MqttClient AmqttClient(wClient);
 
 #define MSG_BUFFER_SIZE 50
 char msg[MSG_BUFFER_SIZE];
+
+bool led_state = false;
 
 void setup_wifi() {
     delay(10);
@@ -58,18 +67,22 @@ void setup_wifi() {
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
+    StaticJsonDocument<256> doc;
+    deserializeJson(doc, payload, length);
     Serial.print("Message arrived [");
     Serial.print(topic);
     Serial.print("] ");
     for (int i = 0; i < length; i++) {
         Serial.print((char)payload[i]);
     }
-    Serial.println();
+    led_state = not led_state;
+    Serial.print(F(", LED:"));
+    Serial.println(led_state);
 }
 
 void reConnect() {
     while (!mqttClient.connected()) {
-        Serial.print("Attempting MQTT connection...");
+        Serial.print(F("Attempting MQTT connection..."));
         // Create a random client ID.
         String clientId = "ipfn/rega-";
         clientId += String(random(0xffff), HEX);
@@ -79,7 +92,7 @@ void reConnect() {
             // Once connected, publish an announcement to the topic.
             mqttClient.publish("ipfn/rega/rp", "hello world");
             // ... and resubscribe.
-            mqttClient.subscribe("ipfn/rega/rp");
+            mqttClient.subscribe("ipfn/rega/led");
         } else {
             Serial.print("failed, rc=");
             Serial.print(mqttClient.state());
@@ -89,6 +102,7 @@ void reConnect() {
     }
 }
 
+// The normal, core0 setup
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
     Serial.begin(115200);
@@ -101,7 +115,6 @@ void setup() {
 
 void loop() {
     static  unsigned long lastMsg = 0;
-    static bool led_state = false;
     unsigned long now = millis();
 
     if (!mqttClient.connected()) {
@@ -117,10 +130,41 @@ void loop() {
         mqttClient.publish("ipfn/rega/rp", msg);  // Publishes a message to the specified
         timeClient.update();
 
-        Serial.println(timeClient.getFormattedTime());
+//        Serial.println(timeClient.getFormattedTime());
 
-        led_state = not led_state;
         digitalWrite(LED_BUILTIN, led_state);
     }
 
 }
+
+// Running on core1
+void setup1() {
+    delay(5000);
+    Serial.printf("C1: Red leader standing by...\n");
+}
+
+void loop1() {
+    static  unsigned long lastMsg = 0;
+    unsigned long now = millis();
+  //time_t t_now;
+  //char buff[80];
+
+
+    if (now - lastMsg > 5000) {
+        lastMsg = now;
+
+        //time(&t_now);
+        //strftime(buff, sizeof(buff), "%c", localtime(&t_now));
+        
+        float coreTemp =analogReadTemp();
+        //Serial.printf("Core temperature %2.1fC ", coreTemp);
+        //Serial.println(buff);
+        snprintf(msg, MSG_BUFFER_SIZE, "Core temperature: %2.1fC", coreTemp);
+
+        mqttClient.publish("ipfn/rega/temp", msg);
+        Serial.println(msg);
+
+    }
+    //delay(500);
+}
+
