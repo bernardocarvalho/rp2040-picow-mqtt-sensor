424a15d830c1a4e53d2fc7327d609b2cf85dc771
Carvalho, Bernardo (bcar) <bernardo.carvalho@tecnico.ulisboa.pt>
2022-12-21 00:24:56 +0000

    Using EEPROM
    Signed-off-by: Carvalho, Bernardo (bcar) <bernardo.carvalho@tecnico.ulisboa.pt>

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,13 @@
 #include <ArduinoMqttClient.h>
 #include <ArduinoJson.h>
 #include <ArduinoOTA.h>
+#include <EEPROM.h>
+
+//#include <PicoOTA.h>
+#include <LittleFS.h>
+
+//#define SECRET_SSID ""
+//#define SECRET_PASS ""
 
 #ifndef SECRET_SSID
 #include "arduino_secrets.h"
@@ -28,20 +35,24 @@ const char *password = SECRET_PASS;
 
 const int HL_PIN = A0; // PICO pin 31
 const int HR_PIN = A1; // PICO pin 32
-const int relayPin = 22; // PICO pin 292
+const int relayPin = 22; // PICO pin 29
 
-const int msgPeriod = 5 * 1000U;
+const int msgPeriod = 15 * 1000U;
 const int wifiPeriod = 300 * 1000U;
-const int ledPeriod = 2 * 1000U;
+int ledPeriod = 2 * 1000UL;
 
 const char* mqttBroker = "test.mosquitto.org";
 const int mqtt_port = 1883;  // Sets the server details.
 // const char* mqtt_server = "91.121.93.94";
 const char* ntpServer = "ntp1.tecnico.ulisboa.pt";
 // int        port        = 1883;
-const char willTopic[] = "ipfn/will";
+const char willTopic[] = "ipfn/picoW/will";
 const char inTopic[]   = "ipfn/picoW/in";
 const char outTopic[]  = "ipfn/picoW/out";
+String willPayload = "oh no!, lost Mqtt ";
+bool willRetain = true;
+int willQos = 1;
+int subscribeQos = 1;
 
 WiFiClient wClient;
 
@@ -62,16 +73,59 @@ const int daylightOffset_sec = 0; // 3600;
 #define WIFI_RETRY 20
 
 #define MSG_BUFFER_SIZE 50
-char msg[MSG_BUFFER_SIZE];
 int wifiOK = 0;
+int wifiRetries = 0 ;
 bool ntpOK = false;
 bool relayState = false;
 
-void reconnect_wifi() {
-    delay(10);
-    if (WiFi.status() == WL_CONNECTED)
-        return;
+void onMqttMessage(int messageSize);
 
+void setup_mqtt(){
+    mqttClient.stop();
+    delay(20);
+    mqttClient.beginWill(willTopic, willPayload.length(), willRetain, willQos);
+    mqttClient.print(willPayload);
+    mqttClient.endWill();
+
+    Serial.print("Attempting to connect to the MQTT broker: ");
+    Serial.println(mqttBroker);
+
+    if (!mqttClient.connect(mqttBroker, mqtt_port)) {
+        Serial.print("MQTT connection failed! Error code = ");
+        Serial.println(mqttClient.connectError());
+
+    }
+
+    Serial.println("You're connected to the MQTT broker!");
+
+    // set the message receive callback
+    mqttClient.onMessage(onMqttMessage);
+
+    Serial.print("Subscribing to topic: ");
+    Serial.println(inTopic);
+    Serial.println();
+
+    // subscribe to a topic
+    // the second parameter sets the QoS of the subscription,
+    // the the library supports subscribing at QoS 0, 1, or 2
+
+    mqttClient.subscribe(inTopic, subscribeQos);
+    // topics can be unsubscribed using:
+    // mqttClient.unsubscribe(inTopic);
+}
+void reconnect_wifi(unsigned long nH2O) {
+    delay(10);
+    if (WiFi.status() == WL_CONNECTED && mqttClient.connected() == 1)
+        return;
+    if(wifiRetries++ > 10){
+        EEPROM.put(0x10, nH2O);
+        if (EEPROM.commit()) {
+            Serial.println("EEPROM successfully committed");
+        } else {
+            Serial.println("ERROR! EEPROM commit failed");
+        }
+        rp2040.reboot();
+    }
     wifiOK = 0;
     // We start by connecting to a WiFi network
     Serial.println();
@@ -82,7 +136,8 @@ void reconnect_wifi() {
     WiFi.begin(ssid, password);
 
     led_state = false;
-    led_blink = false;
+    led_blink = true;
+    ledPeriod = 500UL;
     for(int i = 0; i < WIFI_RETRY; i++){
         //while (WiFi.status() != WL_CONNECTED)
         if (WiFi.status() != WL_CONNECTED) {
@@ -94,14 +149,17 @@ void reconnect_wifi() {
         }
         else {
             wifiOK = 1;
+            wifiRetries = 0;
             led_state = true;
-            led_blink = true;
+            //led_blink = true;
+            ledPeriod = 2000UL;
             //timeClient.begin();
             Serial.println("");
             Serial.println("WiFi connected");
             Serial.println("IP address: ");
             Serial.println(WiFi.localIP());
             digitalWrite(LED_BUILTIN, led_state);
+            setup_mqtt();
             break;
         }
     }
@@ -136,17 +194,22 @@ void onMqttMessage(int messageSize) {
     Serial.print(mqttClient.messageRetain() ? "true" : "false");
     Serial.print("', length ");
     Serial.print(messageSize);
-    Serial.println(" bytes:");
+    //Serial.print(", msg:");
 
     deserializeJson(doc, mqttClient);
 
-    Serial.print(", Pump: ");
+    //Serial.print("Msg: ");
+    //Serial.println((char)mqttClient.read());
     int pump = doc["pump"];
     unsigned long now = millis();
+    time_t nowTime = time(nullptr);
+    unsigned long ntpTime = nowTime;
     if (pump == 1){
         nextWater = now + 2000UL;
     }
-    Serial.print(pump);
+    Serial.print("\"pump\":");
+    Serial.println(pump);
+    //Serial.println();
 
     // use the Stream interface to print the contents
     /*
@@ -154,7 +217,6 @@ void onMqttMessage(int messageSize) {
        Serial.print((char)mqttClient.read());
        }
        */
-    Serial.println();
 
 }
 // The normal, core0 setup
@@ -168,52 +230,21 @@ void setup() {
     Serial.begin(115200);
     // Set in station mode
     WiFi.mode(WIFI_STA);
-    reconnect_wifi();
+    reconnect_wifi(0);
 
-    String willPayload = "oh no!";
-    bool willRetain = true;
-    int willQos = 1;
-
-    mqttClient.beginWill(willTopic, willPayload.length(), willRetain, willQos);
-    mqttClient.print(willPayload);
-    mqttClient.endWill();
-
-    Serial.print("Attempting to connect to the MQTT broker: ");
-    Serial.println(mqttBroker);
-
-    if (!mqttClient.connect(mqttBroker, mqtt_port)) {
-        Serial.print("MQTT connection failed! Error code = ");
-        Serial.println(mqttClient.connectError());
-
-        //        while (1);
-    }
-
-    Serial.println("You're connected to the MQTT broker!");
-
-    // set the message receive callback
-    mqttClient.onMessage(onMqttMessage);
-
-    Serial.print("Subscribing to topic: ");
-    Serial.println(inTopic);
-    Serial.println();
-
-    // subscribe to a topic
-    // the second parameter sets the QoS of the subscription,
-    // the the library supports subscribing at QoS 0, 1, or 2
-    int subscribeQos = 1;
-
-    mqttClient.subscribe(inTopic, subscribeQos);
-
-    // topics can be unsubscribed using:
-    // mqttClient.unsubscribe(inTopic);
     setClockNtp(10000);
 
-
-    Serial.println("End setup()");
     ArduinoOTA.setHostname("PicoW");  // Set the network port name.  设置网络端口名称
     ArduinoOTA.setPassword("666666");  // Set the network port connection
     ArduinoOTA.begin();            // Initialize the OTA.  初始化OTA
     Serial.println("OTA ready!");  // M5.lcd port output format str§
+    nextWater =  millis() + 24UL * 3600UL * 1000UL; // start next day
+    EEPROM.begin(256);
+    unsigned long val;
+    EEPROM.get(0x10, val);
+
+    Serial.print("End setup(). Next H20 ");
+    Serial.println(val);
     delay(20);
 }
 
@@ -221,6 +252,7 @@ void loop() {
     static  unsigned long lastMsg = 0;
     static  unsigned long lastWifiCheck = 0;
     static  unsigned long lastLed = 0;
+    char msg[MSG_BUFFER_SIZE];
 
     StaticJsonDocument<256> doc;
     struct tm timeinfo;
@@ -231,6 +263,8 @@ void loop() {
 
     unsigned long now = millis();
     time_t nowTime = time(nullptr);
+    unsigned long ntpTime = nowTime;
+    gmtime_r(&nowTime, &timeinfo);
 
     if (now > nextWater){
         nextWater =  now + 24UL * 3600UL * 1000UL; // repeat next day
@@ -238,18 +272,19 @@ void loop() {
         sumWater += waterAutoOn;
         //led_state = true;
         //digitalWrite(M5_LED, led_state);
-        digitalWrite(relayPin, HIGH);
+        relayState = true;
+        digitalWrite(relayPin, relayState);
     }
     if (now > stopPump){
-        digitalWrite(relayPin, LOW);
-        //led_state = false;
+        relayState = false;
+        digitalWrite(relayPin, relayState);
         //digitalWrite(M5_LED, led_state);
     }
 
 
     if (now - lastWifiCheck > wifiPeriod) {
         lastWifiCheck = now;
-        reconnect_wifi();
+        reconnect_wifi(ntpTime);
     }
 
     if(led_blink)
@@ -259,31 +294,44 @@ void loop() {
             digitalWrite(LED_BUILTIN, led_state);
         }
 
+    int rawADC_HR, rawADC_HL;
     if (now - lastMsg > msgPeriod) {
         lastMsg = now;
-        snprintf(msg, MSG_BUFFER_SIZE, "Water ADC: %ld, %lu",
-                now, nowTime);
+        /*
+        EEPROM.put(0x10, ntpTime);
+        if (EEPROM.commit()) {
+            Serial.println("EEPROM successfully committed");
+        } else {
+            Serial.println("ERROR! EEPROM commit failed");
+        }
+        */
+        rawADC_HR = analogRead(HR_PIN);
+        rawADC_HL = analogRead(HL_PIN);
+        snprintf(msg, MSG_BUFFER_SIZE, "Water ADC HL: %u, ADC HR: %u, %lu, ",
+                rawADC_HL, rawADC_HR, ntpTime);
         Serial.print(msg);
-        Serial.print(" Wifi:");
-        Serial.println(wifiOK);
+        strftime(msg, MSG_BUFFER_SIZE, "%Y-%m-%dT%H:%M:%SZ", &timeinfo);
+        Serial.print(msg);
+        Serial.print(", Wifi:"); Serial.print(wifiOK);
+        Serial.print(" mqttClient: "); Serial.print(mqttClient.connected());
+        Serial.print(" relay: "); Serial.println(relayState);
 
         bool retained = false;
         int qos = 1;
-        bool dup = false;
+        // bool dup = false;
 
-        float coreTemp =analogReadTemp();
+        float coreTemp = analogReadTemp();
 
-        doc["humidL"] = analogRead(HL_PIN);
-        doc["humidR"] = analogRead(HR_PIN);
+        doc["humidL"] = rawADC_HL;
+        doc["humidR"] = rawADC_HR;
 
-        gmtime_r(&nowTime, &timeinfo);
-        strftime(msg, MSG_BUFFER_SIZE, "%Y-%m-%dT%H:%M:%SZ", &timeinfo);
         doc["time"] = msg;
-        doc["temp"] = coreTemp;
+        doc["tempCore"] = coreTemp;
         doc["count"]   = nowTime;
+        doc["sumWater"] = sumWater;
         //doc["payload"]   = payload;
 
-        mqttClient.beginMessage(outTopic,  (unsigned long)measureJson(doc), retained, qos, dup);
+        mqttClient.beginMessage(outTopic,  (unsigned long) measureJson(doc), retained, qos, false);
         serializeJson(doc, mqttClient);
         mqttClient.endMessage();
     }
